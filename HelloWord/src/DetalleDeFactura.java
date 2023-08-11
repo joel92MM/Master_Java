0782b72d0ad7d6f867a15c70b41a2085d585d477
joel92MM <joel_tf_92@hotmail.com>
2023-08-11 19:55:43 +0100

    Se sube el ejercicio de detalle de factura

--- HelloWord/src/DetalleDeFactura.java
+++ HelloWord/src/DetalleDeFactura.java
@@ -1,4 +1,18 @@
 import java.util.Scanner;
+/*
+
+La tarea consiste en crear una nueva clase con el método main llamada DetalleDeFactura, se requiere desarrollar un programa que reciba datos de la factura utilizando la clase Scanner de la siguiente manera:
+
+        Reciba el nombre de la factura o descripción, utilizar método nextLine() para obtener el nombre de la factura con espacios.
+
+        Reciba 2 precios de productos de tipo double, utilizar método nextDouble() para obtener precios con decimales (,).
+
+        Calcule el total, sume ambos precios y agregue un valor de impuesto del 19%
+
+        Se pide mostrar en un solo String el nombre de la factura, el monto total bruto (antes de impuesto), el impuesto y el monto total neto incluyendo impuesto.
+
+*/
+
 
 public class DetalleDeFactura {
     public static void main(String[] args) {
@@ -22,6 +36,5 @@ public class DetalleDeFactura {
         System.out.println("La factura "+ nombre+ " tiene un total bruto de "+
                 totalBruto+ "€ , con un impuesto de "+ impuesto+ "€ y el monto después" +
                 "de impuesto es de "+totalNeto+"€");
-
     }
 }
